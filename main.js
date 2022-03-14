51d1257a7f15e3cd3f87762c36c33ac0d2750590
Kawsar ahmed <91801080+developer-kawsar1@users.noreply.github.com>
2022-03-14 15:02:14 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+#capture {
+  background-color: #b2f5ea;
+  padding: 1em;
+  height: 400px;
+}
+
+.wp-block-button__link {
+  background-color: #0073aa;
+  color: #fff;
+  transition: background 150ms ease-in-out;
+  border: none;
+  font-size: 0.88889em;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+  line-height: 1.2;
+  box-sizing: border-box;
+  font-weight: bold;
+  text-decoration: none;
+  padding: 0.76rem 1rem;
+  outline: none;
+  outline: none;
+}
+
+img.emoji {
+  display: inline !important;
+  border: none !important;
+  height: 1em !important;
+  width: 1em !important;
+  margin: 0 .07em !important;
+  vertical-align: -0.1em !important;
+  background: none !important;
+  padding: 0 !important;
+  box-shadow: none !important;
+}
